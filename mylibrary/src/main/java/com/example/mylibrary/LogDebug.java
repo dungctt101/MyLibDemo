05f39f9eebb19ffe0d0f26543f8a7520b835ef6d
tiendung <tiendung221992@gmail.com>
2019-03-14 10:38:50 +0700

    fist

---
+++ mylibrary/src/main/java/com/example/mylibrary/LogDebug.java
@@ -0,0 +1,9 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+public class LogDebug {
+    private  static  final  String  TAG ="DEMO";
+    public  static  void d(String messeger){
+        Log.d("",messeger);
+    }
+}
